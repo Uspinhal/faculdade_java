fa8085d792d6e2d3895e27c6ece1887cf3f015e2
Uspinhal <uspinhal@yahoo.com>
2018-10-31 19:01:37 -0300

    Semana 2 - Exercício de apoio 6

--- ConversorMoedas.java
+++ ConversorMoedas.java
@@ -5,7 +5,7 @@ PROGRAMAÇÃO DE COMPUTADORES
 SEMANA 2
 EXERCICIO DE APOIO 6
 
-PROGRAMA PARA CONVERAÕ DE MOEDAS - REAL [R$] PARA DOLAR[US$]
+PROGRAMA PARA CONVERSÃO DE MOEDAS - REAL [R$] PARA DOLAR[US$]
 
 */
 
@@ -28,7 +28,7 @@ class ConversorMoedas{
 
         System.out.println("Entre com o valor em Real: R$ ");
         real = input.nextFloat();
-        System.out.println("Entre com a contacao do Dolar: R$ ");
+        System.out.println("Entre com a cotacao do Dolar: R$ ");
         dolar = input.nextFloat();
 
         System.out.println("Valor convertido: US$ " + conversor(real, dolar));
